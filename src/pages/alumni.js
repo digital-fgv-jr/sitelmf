d423ae212d0c95c86c176790e22a01a397f753fc
theBatista <filho.antonio@fgv.edu.br>
2025-03-23 22:30:55 -0300

    alumni finished

--- src/pages/alumni.js
+++ src/pages/alumni.js
@@ -1,10 +1,214 @@
-function alumni() {
-    return (
-      <div>
-        <h1>alumni</h1>
-        <p>teste</p>
+import React from 'react';
+import '../styles/pages/alumni.css';
+
+function Alumni() {
+  return (
+    <div className='ps-container'>
+
+      <div className="ps-header">
+        <img src="/imagem-inicio.jpg" alt="Imagem topo" className="ps-imagem-topo" />
+        <div className="ps-banner">
+          <div className='ps-logo-banner'>
+            <div className="ps-logo">
+          <img
+            src="/logoo.png"
+            alt="Logo LMF"
+            className="ps-logo-image"
+          />
+          <div className="ps-separator"></div>
+          <div className="ps-logo-name">
+            <span className="ps">Alumni</span>
+          </div>
+        </div>
+          </div>
+          <div className="ps-tela-azul"></div>
+        </div>
       </div>
-    );
-  }
-  
-  export default alumni;
+
+      <div className='profile-container'>
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+
+        <div className="profile">
+          <div className="profile-header">
+              <span className="name">Nome Sobrenome</span>
+              <a href="https://www.linkedin.com" target="_blank" class="linkedin-icon">
+                  <img src="linkedin-icon-azul.png" alt="LinkedIn" />
+              </a>
+          </div>
+          <div className="position">Posição</div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default Alumni;
