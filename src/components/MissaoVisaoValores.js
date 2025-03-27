3f8cd3668818bda137d0d07cc8b93db779f5acb7
hpetrillo <169162341+hpetrillo@users.noreply.github.com>
2025-03-27 17:49:24 +0000

    MVV

--- src/components/MissaoVisaoValores.js
+++ src/components/MissaoVisaoValores.js
@@ -8,19 +8,25 @@ function MissoesVisaoValores() {
       <div className='subitem-missao-visao-valores'>
         <h3>Missão</h3>
         <p>
-          Descrição da missão da organização. Descrição da missão da organização. Descrição da missão da organização.
-          Descrição da missão da organização. Descrição da missão da organização. Descrição da missão da organização.
+        Ser uma comunidade acadêmica que inspira e prepara seus membros para carreiras sólidas no mercado financeiro, destacando-se pela aplicação prática do conhecimento e pela construção contínua de oportunidades concretas para nossos integrantes.
         </p>
       </div>
 
       <div className='subitem-missao-visao-valores'>
         <h3>Visão</h3>
-        <p>Descrição da visão da organização.</p>
+        <p>Estimular e capacitar nossos membros por meio da combinação equilibrada entre teoria e desafios práticos. Promovemos eventos, treinamentos e contato direto com profissionais do mercado, visando desenvolver habilidades essenciais e fomentar o pensamento crítico necessário para o sucesso no setor financeiro.
+        </p>
       </div>
 
       <div className='subitem-missao-visao-valores'>
         <h3>Valores</h3>
-        <p>Descrição dos valores da organização.</p>
+        <p>
+          Meritocracia: reconhecimento pelo esforço e resultados alcançados.<br />
+          Excelência: busca constante por qualidade e alto desempenho.<br />
+          Pensamento crítico: estímulo ao questionamento, análise e aprendizado contínuo.<br />
+          Resiliência: capacidade de enfrentar e superar desafios em um mercado competitivo.<br />
+          Comunidade: construção de uma rede colaborativa e de crescimento mútuo.<br />
+</p>
       </div>
     </div>
   );
