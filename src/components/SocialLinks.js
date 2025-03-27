bf94469b709659a6b99fd25e326b2782f8c8bcc0
theBatista <filho.antonio@fgv.edu.br>
2025-03-26 21:17:53 -0300

    changing to ra icons

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { FaInstagram, FaLinkedin } from 'react-icons/fa';
 import '../styles/components/SocialLinks.css';
 
-const SocialLinks = ({color = 'white'}) => {
+const SocialLinks = ({ color = 'white' }) => {
   return (
-    <div className="SocialLinks">
-        <a href="https://www.instagram.com/lmfgvrio" target="_blank" rel="noopener noreferrer">
-            <i className={`fab fa-instagram social-icon ${color}`}></i>
-        </a>
-        <a href="https://www.linkedin.com/company/liga-de-mercado-financeiro-fgv-rio" target="_blank" rel="noopener noreferrer">
-            <i className={`fab fa-linkedin social-icon ${color}`}></i>
-        </a>
+    <div className='SocialLinks'>
+      <a
+        href='https://www.instagram.com/lmfgvrio'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <FaInstagram className={`social-icon ${color}`} />
+      </a>
+      <a
+        href='https://www.linkedin.com/company/liga-de-mercado-financeiro-fgv-rio'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        <FaLinkedin className={`social-icon ${color}`} />
+      </a>
     </div>
   );
 };
